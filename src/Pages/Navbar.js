6296b3e98a354e672d128b02898f0e0c59ae1599
Joshua Myers <joshuam7291@gmail.com>
2025-01-13 23:15:20 -0500

    fixed some things

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,19 +1,19 @@
+import { Link } from 'react-router-dom';
 import './Styles/Navbar.css'
 
 export default function Navbar() {
-
     return (
         <div className='nav'>
-                <br></br>
-                <a href='/Home' className='alist'>Home</a>
-                <br></br>
-                <a href='/Survey' className='alist'>Survey</a>
-                <br></br>
-                <a href='/Itinerary' className='alist'>Itinerary</a>
-                <br></br>
-                <a href='/Maps' className='alist'>Maps</a>
-                <br></br>
-                <br></br>
+            <br />
+            <Link to='/Home' className='alist'>Home</Link>
+            <br />
+            <Link to='/Survey' className='alist'>Survey</Link>
+            <br />
+            <Link to='/Itinerary' className='alist'>Itinerary</Link>
+            <br />
+            <Link to='/Maps' className='alist'>Maps</Link>
+            <br />
+            <br />
         </div>
     )
 }
